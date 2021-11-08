b67e1576e321f2f758e09a70e9adddce64d4b0cf
shane <willyums86@gmail.com>
2021-11-08 16:10:45 -0500

    Fixed Backend error

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,8 @@ class User < ApplicationRecord
   has_secure_password
   validates :email, presence: true, uniqueness: true
   validates :name, presence: true, length: { minimum: 5 }
-  validates :password_digest presence: true, uniqueness: true
-  validates :phone_number, presence: true, length: {minimum: 5}
+  # validates :password_digest presence: true, uniqueness: true
+  validates :phone_number, presence: true, length: { minimum: 5 }
   validates :image_url, presence: true
 
   has_many :patients
