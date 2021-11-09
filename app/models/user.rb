e7f8ea1a11806d9af983368990c2d4f3c74698aa
shane <willyums86@gmail.com>
2021-11-09 10:56:34 -0500

    Bug Fixes

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,6 @@ class User < ApplicationRecord
   validates :email, presence: true, uniqueness: true
   validates :name, presence: true, length: { minimum: 5 }
   # validates :password_digest presence: true, uniqueness: true
-  validates :phone_number, presence: true, length: { minimum: 5 }
-  validates :image_url, presence: true
 
   has_many :patients
 end
